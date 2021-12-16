8af21923c900d7ecbafe5b5e046efa86e4de6327
patterso3300 <96032623+patterso3300@users.noreply.github.com>
2021-12-16 11:37:29 +0800

    11
    11324额度特今年

---
+++ _.Xd=function(){if(!_.n.addEventListener.js
@@ -0,0 +1,22 @@
+_.Xd=function(){if(!_.n.addEventListener||!Object.defineProperty)return!1;var a=!1,b=Object.defineProperty({},"passive",{get:function(){a=!0}});try{_.n.addEventListener("test",_.$a,b),_.n.removeEventListener("test",_.$a,b)}catch(c){}return a}();
+_.Yd=_.vb?"webkitTransitionEnd":"transitionend";
+
+}catch(e){_._DumpException(e)}
+try{
+var Zd=document.querySelector(".gb_B .gb_C"),$d=document.querySelector("#gb.gb_Dc");Zd&&!$d&&_.Wd(_.Nd,Zd,"click");
+
+}catch(e){_._DumpException(e)}
+try{
+var Jh=function(a){_.K.call(this);this.C=a;this.A=null;this.o={};this.D={};this.j={};this.B=null};_.u(Jh,_.K);_.Kh=function(a){if(a.A)return a.A;for(var b in a.j)if(a.j[b].mf()&&a.j[b].Sb())return a.j[b];return null};_.h=Jh.prototype;_.h.Gf=function(a){a&&_.Kh(this)&&a!=_.Kh(this)&&_.Kh(this).ae(!1);this.A=a};_.h.Lg=function(a){a=this.j[a]||a;return _.Kh(this)==a};_.h.Ve=function(a,b){b=b.Wc();if(this.o[a]&&this.o[a][b])for(var c=0;c<this.o[a][b].length;c++)try{this.o[a][b][c]()}catch(d){this.C.log(d)}};
+_.h.Yh=function(a){return!this.D[a.Wc()]};_.h.ih=function(a){this.j[a]&&(_.Kh(this)&&_.Kh(this).Wc()==a||this.j[a].ae(!0))};_.h.$a=function(a){this.B=a;for(var b in this.j)this.j[b].mf()&&this.j[b].$a(a)};_.h.Cf=function(a){this.j[a.Wc()]=a};_.h.Me=function(a){return a in this.j?this.j[a]:null};var Lh=new Jh(_.Ld);_.md("dd",Lh);
+
+}catch(e){_._DumpException(e)}
+try{
+var nj=document.querySelector(".gb_Na .gb_C"),oj=document.querySelector("#gb.gb_Dc");nj&&!oj&&_.Wd(_.Nd,nj,"click");
+
+}catch(e){_._DumpException(e)}
+})(this.gbar_);
+// Google Inc.
+;this.gbar_=this.gbar_||{};(function(_){var window=this;
+try{
+/*
